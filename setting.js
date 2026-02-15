7f095f9e275c54e57cf06bcdb2c29b82b4fb4768
fathir <fathirsthore@gmail.com>
2026-02-15 21:27:46 +0700

    Update payment API URLs and Pterodactyl configuration

--- setting.js
+++ setting.js
@@ -12,20 +12,20 @@ global.mongodbURI = 'mongodb+srv://website:038jYER6jKUDqxqC@cluster0.2c2cpjd.mon
 // Payment API Configuration
 // QRIS A - Atlantik
 global.atlantikApiKey = '56j5JPPoiibcmyEQ5FOPsrPj8UnsXxId' // Isi di admin panel
-global.atlantikBaseURL = 'https://atlantik.top/api'
+global.atlantikBaseURL = 'https://atlantich2h.com'
 
 // QRIS B - Pakasir
 global.pakasirApiKey = 'RjKJLZZ85o3xlTSlNOiR85mI6b2UZgHF0gUI9pKsegqMDPTI5SYyLebs7Lsy1GpW5LHtG47IFC6eDrK1dDNWNmE3Q3WdStDGjzEC' // Isi di admin panel
-global.pakasirBaseURL = 'https://pakasir.com/api'
+global.pakasirBaseURL = 'https://app.pakasir.com/api'
 
 // Default Payment Provider (atlantik / pakasir)
 global.defaultPaymentProvider = 'atlantik'
 
 // Panel Pterodactyl Configuration
-global.plta = '' // API Key Panel - Isi di admin panel
-global.pltc = '' // Client API Key - Isi di admin panel
-global.domain = '' // Domain Panel - Isi di admin panel
-global.loc = '1' // Location ID
+global.plta = 'ptla_MOk2MyqDrKXMDFXQWwLzGXfTMhIxo551hKbdj0pc6Fz' // API Key Panel - Isi di admin panel
+global.pltc = 'ptlc_vtlDeJ2SZqZCgPJ225wHXGgZNZLe2fQGGVqk6Ofqsm8'// Client API Key - Isi di admin panel
+global.domain = 'https://hanzkecemyprivate.panel-xsat.my.id' // Domain Panel - Isi di admin panel
+global.loc = '15' // Location ID
 global.eggs = '1' // Egg ID
 
 // Fee Configuration
